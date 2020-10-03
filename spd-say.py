c603dc314ebf9f182c13caba1f201c391062f6af
mrsparkl3 <ste.gambardella@gmail.com>
2020-10-03 17:21:53 +0200

    added quit function and help messages

--- spd-say.py
+++ spd-say.py
@@ -4,8 +4,15 @@ import os
 #simple python script that makes it easier to use spd-say command on 
 # Ubuntu. Make sure to add a proper alias to the bashrc file
 # to run this script.
+help= "\nSpd-say facilitator user, \nJust type something and your pc will say it.\nType \"quit\" to exit\n"
+print(help)
 while True:
 	text= input("say something>")
-	cmd= "spd-say \'" +text + "\'"
-	os.system(cmd)
-	
+	if text == "quit":
+		break
+	elif text == "help":
+		print(help)
+	else:
+		cmd= "spd-say \'" +text + "\'"
+		os.system(cmd)
+		
