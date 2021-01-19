5fd603be464382b8f2782528d95663f423c0adbd
mrsparkl3 <ste.gambardella@gmail.com>
2021-01-19 17:59:50 +0100

    tuples

---
+++ tuple.py
@@ -0,0 +1,8 @@
+programming_languages = "Python", "Java", "PhP"
+# can even be: programming_languages = ("Python", "Java", "PhP")
+
+#print(type(programming_languages))
+# same result: it's a tuple
+
+for language in programming_languages:
+  print(language)
